a6a1494fea31e26e2794bda6933951b63bc149fc
Zimo Dong <andy199354@gmail.com>
2017-12-02 16:09:00 -0800

    added the logic for previous and next buttons

--- js/app.js
+++ js/app.js
@@ -13,10 +13,10 @@ const getEmployees = (item)=>{//call back function
 
   $.each(employee_results,function(index, employee){
     //for employeeHTML
-    employeesHTML += '<li><a href = "#myModal " class = "open_modal">';
+    employeesHTML += '<li><a href = "#myModal " class = "open_modal" id="' + "employee_"+ index + '">';
     employeesHTML += '<img src = "' + employee.picture.large + '">';
     employeesHTML += '<ul class = "info">';
-    employeesHTML += '<li class = "emp_name">' + employee.name.first + '</li>';
+    employeesHTML += '<li class = "emp_name" id = "' + "name_" + index + '">' + employee.name.first + '</li>';
     let emailStr = '';
     if(employee.email.length >= 33){//too long
       emailStr += employee.email.substring(0,4);
@@ -57,7 +57,7 @@ const getEmployees = (item)=>{//call back function
     let modal_phone = $(this).find(".emp_phone").html();//related employee phone
     let modal_addr = $(this).find(".emp_addr").html();//related employee addr
     let modal_BDay = $(this).find(".emp_BDay").html();//related employee BDay
-    //console.log(modal_name);
+
     let modalHTML = '';
     modalHTML += '<span class="close">&times;</span>';//add close span
     modalHTML += '<br><br>';//add line breaks
@@ -67,7 +67,7 @@ const getEmployees = (item)=>{//call back function
     modalHTML += '<img src = "' + modal_img + '" class = "modal-img">';
 
     modalHTML += '<ul class = "modal_list_1">';//ul for list1
-    modalHTML += '<li class = emp_name>' + modal_name +'</li>'; //add li name
+    modalHTML += '<li class = emp_name id ="' + $(this).find(".emp_name").attr("id") + '" >' + modal_name +'</li>'; //add li name also record the id
     modalHTML += '<li class = emp_email>' + modal_email +'</li>';//add li email
     modalHTML += '<li class = emp_city>' + modal_city +'</li>';//add li city
     modalHTML +='</ul>';//close ul for list1
@@ -90,6 +90,32 @@ const getEmployees = (item)=>{//call back function
   //It won't get bound to elements created in the future. To do that, you'll have to create a "delegated" binding by using on().
   $(document).on("click",".close",function(){
     $modal.css("display","none");
+  });
+
+ //logic for previous and next button
+  $(document).on("click",".btn",function(){
+    $modal.css("display","none");//close the current modal
+    let currentIndex = $(this).parent().find(".emp_name").attr("id");//the employee index for current opened modal ep. name_3
+    console.log(currentIndex);
+    let Indexnumber = parseInt(currentIndex.split("_").pop());//get the actual index number
+    let AfterBtnIndex = '';//correct index after pressing ther prev or next button
+    if($(this).hasClass("previous_btn")){//means the previous button is clicked
+      if(Indexnumber === 0){//cant previous anymore
+        AfterBtnIndex = "name_" + Indexnumber;
+      }else{  //can go previous
+        AfterBtnIndex = "name_" + (Indexnumber-1);
+      }
+      console.log(AfterBtnIndex);
+    }else{//the next button is clicked
+      if(Indexnumber === 11){//cant go next anymore
+        AfterBtnIndex = "name_" + Indexnumber;
+      }else{  //can go next
+        AfterBtnIndex = "name_" + (Indexnumber+1);
+      }
+      console.log(AfterBtnIndex);
+    }
+
+   $('.clearfix li').find("#" + AfterBtnIndex).trigger("click");//open the new modal with correct index
   });
 
   //When the user clicks anywhere outside of the modal, close it
